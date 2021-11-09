f5b83b90f2ba698c0c14eee3f4134c5c08d4bece
shayjung <sosat4425@gmail.com>
2021-11-09 10:00:59 +0900

    카테고리 상세페이지 메뉴 변경 (수정중)

--- public/javascripts/classView.js
+++ public/javascripts/classView.js
@@ -1,10 +1,19 @@
-// $(function() {
-//
-//   $('li').click(function() {
-//     $('li').removeClass()
-//       $(this).addClass('on')
-//   })
-// })
+$(function() {
+  //
+  // $(".category-list li").click(function() {
+  //     $(".category-list li").removeClass()
+  //       $(this).addClass("on");
+  // })
+  var tabBtn = $(".category-list > li");     //각각의 버튼을 변수에 저장
+
+
+  tabBtn.click(function(){
+    var target = $(this);         //버튼의 타겟(순서)을 변수에 저장
+    var index = target.index();   //버튼의 순서를 변수에 저장
+    tabBtn.removeClass("on");    //버튼의 클래스를 삭제
+    target.addClass("on");       //타겟의 클래스를 추가
+  });
+});
 
 $(document).ready(function($) {
 
