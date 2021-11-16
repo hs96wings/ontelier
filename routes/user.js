4e3328e6474c9b4f2fc78382f6e2eaf84f2b1772
hs96wings <hs96wings@gmail.com>
2021-11-16 15:34:25 +0900

    마이페이지 구매내역 갯수

--- routes/user.js
+++ routes/user.js
@@ -9,24 +9,49 @@ const moment = require('moment');
 
 const router = express.Router();
 
-router.get('/', isLoggedIn, (req, res, next) => {
-    Class.findAll({
+// router.get('/', isLoggedIn, (req, res, next) => {
+//     Class.findAll({
+//         include: {
+//             model: Purchase,
+//             where: {UserUserId: req.user.user_id}
+//         },
+//     })
+//     .then((result) => {
+//         res.render('mypage', {
+//             title: '온뜰 - Mypage',
+//             classes: result,
+//         });
+//     })
+//     .catch((error) => {
+//         req.flash('error', '오류가 발생했습니다');
+//         res.redirect('/');
+//     });
+// });
+
+router.get('/', isLoggedIn, async (req, res) => {
+    const purchases = await Class.findAndCountAll({
         include: {
             model: Purchase,
             where: {UserUserId: req.user.user_id}
         },
-    })
-    .then((result) => {
+    });
+    if (purchases) {
+        let wishlist_num = await Wishlist.count({where: {UserUserId: req.user.user_id}});
+        let review_num = await Review.count({where: {UserUserId: req.user.user_id}});
+        if (!wishlist_num) wishlist_num = 0;
+        if (!review_num) review_num = 0;
         res.render('mypage', {
             title: '온뜰 - Mypage',
-            classes: result,
+            classes: purchases.rows,
+            purchase_num: purchases.count,
+            wishlist_num: wishlist_num,
+            review_num: review_num,
         });
-    })
-    .catch((error) => {
-        req.flash('error', '오류가 발생했습니다');
+    } else {
+        req.flash('error', 'DB 오류');
         res.redirect('/');
-    });
-});
+    }
+})
 
 router.get('/edit', isLoggedIn, (req, res, next) => {
     res.render('userinfo_update', {title: '온뜰 - mypage 수정'});
