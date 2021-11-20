73fe1b20f8c43bc2b5e4b38ef33b4115c46b98b0
shayjung <sosat4425@gmail.com>
2021-11-21 07:02:28 +0900

    메인 배너 수정

--- public/javascripts/slider.js
+++ public/javascripts/slider.js
@@ -1,7 +1,7 @@
 $(function(){
 
   $('.autoplay').slick({
-  	slide: 'img',		//슬라이드 되어야 할 태그 ex) div, li
+  	slide: 'div',		//슬라이드 되어야 할 태그 ex) div, li
   	infinite : true, 	//무한 반복 옵션
   	slidesToShow : 1,		// 한 화면에 보여질 컨텐츠 개수
   	slidesToScroll : 1,		//스크롤 한번에 움직일 컨텐츠 개수
