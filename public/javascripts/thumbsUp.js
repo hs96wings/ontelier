34093453e21b30d90a47b40c306dcf708bb1b173
shayjung <sosat4425@gmail.com>
2021-11-26 23:11:24 +0900

    도움이 됐어요 / 찜목록 버튼 바로 변경되도록 수정

--- public/javascripts/thumbsUp.js
+++ public/javascripts/thumbsUp.js
@@ -10,8 +10,14 @@ function thumbsUp(classId, id) {
       dataType: "json",
       success: function(response) {
         console.log(response.message);
-        $(`#review_id${id} .thumbsup-text img`).replaceWith(`<img src="/images/thumbs-up-click.png">`) // 이미지 변경
-        $(`#review_id${id} #thumbsup-num`).replaceWith(`<span id="thumbsup-num>${response.num}</span>"`); // 서버로부터 1 증가된 값 받아서 넣기
+        if ($(`#review_id${id} #like-cnt`).attr("class") == "unchecked") { // 클래스명이 unchecked 일 시
+          $(`#review_id${id} .thumbsup-text img`).replaceWith(`<img src="/images/thumbs-up-click.png">`); // 이미지 변경
+          $(`#review_id${id} #like-cnt`).attr("class", "checked"); // 클래스명 checked로 변경
+        } else {
+          $(`#review_id${id} .thumbsup-text img`).replaceWith(`<img src="/images/thumbs-up.png">`); // 이미지 변경
+          $(`#review_id${id} #like-cnt`).attr("class", "unchecked"); // 클래스명 unchecked로 변경
+        }
+        $(`#review_id${id} #thumbsup-num`).html(response.num); // 서버로부터 변경된 값 받아서 넣기
       },
       error: function (request, status, error) {
         alert(error);
