c2c58d6265245073ce295d3c8a553fe65323ee80
shayjung <sosat4425@gmail.com>
2021-12-08 04:08:54 +0900

    후기 필터 ajax 구현 / 도움이 됐어요 모델 수정

--- public/javascripts/thumbsUp.js
+++ public/javascripts/thumbsUp.js
@@ -1,5 +1,29 @@
+function parseThumbsUp() {
+  var mydata = "";
+  $.ajax({
+        type : "GET",
+        url : `/mypage/thumbsup`,
+        dataType : "json",
+        async: false,
+        error : function(request, status, error){
+            console.log(error);
+        },
+        success : function(Thumb_data){
+
+          mydata = Thumb_data.Thumbs_data.map(a => a.ReviewId); // 받아온 좋아요 id 데이터
+
+          console.log(mydata);
+          }
+    });
+
+    return mydata;
+}
+
 // 도움이 됐어요
-function thumbsUp(classId, id) {
+function thumbsUp(classId, id, userId) {
+
+  // 로그인 되어있을 시에만
+  if (userId != undefined) {
     $.ajax({
       type: "POST",
       url: `/class/${classId}/review/like`,
@@ -23,4 +47,23 @@ function thumbsUp(classId, id) {
         alert(error);
       },
     });
+
+  } else {
+    alert("로그인 후에 이용할 수 있습니다.");
+    window.location.href = "/login";
+  }
+
+}
+
+// 도움이 됐어요 눌려있도록
+function ReadyThumbsUp(id, mydata) {
+
+    for (var i = 0; i < mydata.length; i++) {
+      if (mydata[i] == id) {
+        // 유저 찜목록에 이 클래스가 있으면
+          $(`#review_id${id} .thumbsup-text #like-cnt`).attr("class", "checked"); // 클래스명 checked로 변경
+          $(`#review_id${id} .thumbsup-text #like-cnt .like-btn`).attr("src", "/images/thumbs-up-click.png"); // 이미지 변경
+      }
+    }
+
 }
