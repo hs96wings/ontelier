2e6ea682bcce970588fe1e5f80f7c7bf5fc08a98
shayjung <sosat4425@gmail.com>
2021-12-13 10:41:18 +0900

    관리자 글쓰기 강사소개 summernote 추가 / 글 수정시 카테고리 값 불러오도록 변경

--- public/javascripts/summerNote.js
+++ public/javascripts/summerNote.js
@@ -1,6 +1,6 @@
 // 썸머노트 실행 스크립트
 
-  $('#summernote').summernote({
+  $('.summernote').summernote({
 		height: 600,
 		width : 1000,
 		minHeight: null,
@@ -35,21 +35,21 @@
 
 
   // 서머노트에 text 쓰기
-  $('#summernote').summernote('insertText', '내용입력');
+  $('.summernote').summernote('insertText', '내용입력');
 
 
   // 서머노트 쓰기 비활성화
-  $('#summernote').summernote('disable');
+  $('.summernote').summernote('disable');
 
   // 서머노트 쓰기 활성화
-  $('#summernote').summernote('enable');
+  $('.summernote').summernote('enable');
 
 
   // 서머노트 리셋
-  $('#summernote').summernote('reset');
+  $('.summernote').summernote('reset');
 
 
   // 마지막으로 한 행동 취소 ( 뒤로가기 )
-  $('#summernote').summernote('undo');
+  $('.summernote').summernote('undo');
   // 앞으로가기
-  $('#summernote').summernote('redo');
+  $('.summernote').summernote('redo');
